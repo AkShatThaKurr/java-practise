a08a269f2030f1526e2790a914f431457c28490b
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 03:57:29 +0530

    Add Arithmetic1 program

---
+++ src/Arithmetic2.java
@@ -0,0 +1,14 @@
+// Program to Subtract two numbers.
+import java.util.Scanner;
+
+public class Arithmetic2 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a,b;
+            System.out.println("Enter two no. to find their difference");
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println("The difference of " + a + " and " + b + " is " + (a-b) + ".");
+        }
+    }
+
