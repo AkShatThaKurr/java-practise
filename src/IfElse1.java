0f96f66289416037b663d91072c2d189a85c8e5e
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 04:04:12 +0530

    Add IfElse1 program

---
+++ src/IfElse1.java
@@ -0,0 +1,18 @@
+// check whether number is even or odd.
+import java.util.Scanner;
+
+public class IfElse1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int a = sc.nextInt();
+
+        if(a%2==0){
+            System.out.println(a + " is Even.");
+        }
+        else{
+            System.out.println(a + " is Odd.");
+        }
+    }
+
+}
