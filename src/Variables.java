2dede31f4f590adb1684ae6876d232f5c5f89061
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 03:15:13 +0530

    Add HelloWorld program Add Variables program

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Akshat";
+        double price = 99.5;
+        // here: age stores number, name stores text, price sotres decimal.
+    }
+}
