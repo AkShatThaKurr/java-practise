4313e754a903ea9535bd98950cf13f3fc04c76f7
Akshat Thakur <codingontheway@gmail.com>
2026-03-15 01:00:05 +0530

    Add Loops01 program

---
+++ src/Loops01.java
@@ -0,0 +1,8 @@
+// Write a program to print numbers from 1 to 10 using for loop
+public class Loops01 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
