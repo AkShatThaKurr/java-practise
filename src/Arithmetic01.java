8337530d50e8c1eeda2c2a644f11e993ea2dbdf6
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 13:57:36 +0530

    Add Arithmetic01 program

--- src/Arithmetic01.java
+++ src/Arithmetic01.java
@@ -1,7 +1,7 @@
 // Program to add two numbers.
 import java.util.Scanner;
 
-public class Arithmetic1 {
+public class Arithmetic01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a,b;
