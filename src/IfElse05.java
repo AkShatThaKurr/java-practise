cb9c9a1bce95c4215c60c8070f4920ad547fd840
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 14:45:28 +0530

    Add IfElse05 program

---
+++ src/IfElse05.java
@@ -0,0 +1,16 @@
+// find greatest of two numbers
+
+public class IfElse05 {
+    public static void main(String[] args) {
+        int num1=100, num2=500;
+        if(num1>num2){
+            System.out.println(num1);
+        }
+        else if(num1==num2){
+            System.out.println("Both are equal.");
+        }
+        else{
+            System.out.println(num2);
+        }
+    }
+}
