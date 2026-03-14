53d2e7589cb72f4b35b85c1a327241b0a2ed67cc
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 13:26:24 +0530

    7

---
+++ src/Arithmetic7.java
@@ -0,0 +1,9 @@
+// Program to convert Celsius to Fahrenheit [f = (c *(9/5)) + 32 ]
+
+public class Arithmetic7 {
+    public static void main(String[] args) {
+        double celc = 35;
+        double fah = (celc * (9.0/5)) + 32;
+        System.out.println("f = " + fah);
+    }
+}
