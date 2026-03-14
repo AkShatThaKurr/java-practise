e7afd14f25e6814149ebd1229d0db0eed30e3d48
Akshat Thakur <codingontheway@gmail.com>
2026-03-15 01:08:30 +0530

    Add Loops03 program

---
+++ src/Loops03.java
@@ -0,0 +1,11 @@
+// Write a program to print even numbers from 1 to 20.
+
+public class Loops03 {
+    public static void main(String[] args) {
+        for(int i=1; i<=20; i++){
+            if(i%2==0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
