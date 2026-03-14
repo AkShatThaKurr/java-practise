7bb9df4eefffaeda48759d3a135f41d10fc96eed
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 14:36:43 +0530

    Add IfElse03 program

---
+++ src/IfElse03.java
@@ -0,0 +1,16 @@
+// Check whether number is positive or negative.
+
+public class IfElse03 {
+    public static void main(String[] args) {
+        int a = -10;
+        if(a>0){
+            System.out.println("Positive");
+        }
+        else if(a==0){
+            System.out.println("Zero");
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+}
