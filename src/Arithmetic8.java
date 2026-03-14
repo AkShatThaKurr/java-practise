04d9ea1115df7146a5b2a836cc45b0baf3a20ce0
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 13:44:08 +0530

    Add Arithmetic8 program

---
+++ src/Arithmetic8.java
@@ -0,0 +1,15 @@
+// Program to swap two numbers
+
+public class Arithmetic8 {
+    public static void main(String[] args) {
+        int a=11, b=55;
+        int swap = a;
+        System.out.println("Before Swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+        a = b;
+        b = swap;
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+
+}
