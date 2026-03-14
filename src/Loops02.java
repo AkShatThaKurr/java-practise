e3b3841c4b586583cbeb77a92f83db18d4ca05b9
Akshat Thakur <codingontheway@gmail.com>
2026-03-15 01:06:13 +0530

    Add Loops02 program

---
+++ src/Loops02.java
@@ -0,0 +1,12 @@
+// Using a program to print numbers from 1 to 10 using while loop.
+
+public class Loops02 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10){
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+
+}
