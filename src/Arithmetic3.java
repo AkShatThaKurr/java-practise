5d2d7c068475ddd97febdd0286197e2c9538e3a0
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 13:14:59 +0530

    Add Arithmetic3 program

---
+++ src/Arithmetic3.java
@@ -0,0 +1,8 @@
+// Program to find remainder
+
+public class Arithmetic3 {
+    public static void main(String[] args) {
+        int a = 123;
+        System.out.println("The remainder of " + a + " is " + a%10 + ".");
+    }
+}
