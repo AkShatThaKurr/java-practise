3b9aecacbb9deb46cb389dfe91bde9fd12ae23db
Akshat Thakur <codingontheway@gmail.com>
2026-03-15 01:15:59 +0530

    Add Loops05 program

---
+++ src/Loops05.java
@@ -0,0 +1,13 @@
+// Write a program to print table of a number.
+import java.util.Scanner;
+
+public class Loops05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to print its table: ");
+        int num = sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(num + "x" + i + "=" + num*i);
+        }
+    }
+}
