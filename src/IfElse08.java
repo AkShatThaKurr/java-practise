817bbe93a8b470922c798593e911624e038690b6
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 14:54:37 +0530

    Add IfElse08 program

---
+++ src/IfElse08.java
@@ -0,0 +1,16 @@
+// Check Pass or Fail
+
+public class IfElse08 {
+    public static void main(String[] args) {
+        int marks = 79;
+        if(marks<0 || marks>100){
+            System.out.println("Invalid input");
+        }
+        else if(marks>=30){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
