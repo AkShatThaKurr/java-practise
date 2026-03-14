7a4eb387649a748d2a8257fd1bea51f28d71ef3b
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 13:20:41 +0530

    Add Arithmetic5 program

---
+++ src/Arithmetic4.java
@@ -0,0 +1,11 @@
+// Program to calculate simple interest (si = (p*r*t)/100 )
+
+public class Arithmetic4 {
+    public static void main(String[] args) {
+        int p = 10000;
+        double r = 2.5;
+        double t = 2;
+        double si = (p*r*t)/100;
+        System.out.println("The Simple Interest is: " + si);
+    }
+}
---
+++ src/Arithmetic5.java
@@ -0,0 +1,8 @@
+
+// Program to calculate area of circle
+public class Arithmetic5 {
+    public static void main(String[] args) {
+        int r = 7;
+        System.out.println("Area of circle is: " + Math.PI * r *r );
+    }
+}
