9d921cd22b3047a320f6341b092c0e8d2b0ddf21
Akshat Thakur <codingontheway@gmail.com>
2026-03-14 13:22:17 +0530

    Add Arithmetic6 program

---
+++ src/Arithmetic6.java
@@ -0,0 +1,9 @@
+// calculate area of rectangle
+
+public class Arithmetic6 {
+    public static void main(String[] args) {
+        int l=5, b=4;
+        int area = l*b;
+        System.out.println("Area of rectangle = " + area);
+    }
+}
