70c9897297ac5a36e58f52dc97aefe71091a43fd
Akshat Thakur <codingontheway@gmail.com>
2026-03-15 06:48:55 +0530

    Add Pattern4 program

---
+++ src/Pattern04.java
@@ -0,0 +1,13 @@
+// Write a program to print inverted left triangle.
+
+public class Pattern04 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
