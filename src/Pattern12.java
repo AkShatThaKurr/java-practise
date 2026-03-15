d09960b0e9f49977521bd9ebe2a5a7956aa63a3a
Akshat Thakur <codingontheway@gmail.com>
2026-03-15 07:11:48 +0530

    Add Pattern12 program

---
+++ src/Pattern12.java
@@ -0,0 +1,24 @@
+// Write a program to print binarhy triangle pattern.
+/*
+1
+0 1
+1 0 1
+0 1 0 1
+ */
+
+public class Pattern12 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print(1 + " ");
+                }
+                else{
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
