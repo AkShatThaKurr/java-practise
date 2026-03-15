bc81ed033e7dce577e294c3389fdb60f53894694
Akshat Thakur <codingontheway@gmail.com>
2026-03-15 06:43:37 +0530

    Add Pattern1 program

---
+++ src/Pattern01.java
@@ -0,0 +1,14 @@
+// Write a program to print a straight horizontal line of stars. (* * * * *)
+
+public class Pattern01 {
+    public static void main(String[] args) {
+        int n=1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
