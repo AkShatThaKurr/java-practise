1511ecde8b5fd3db91333987dac067d6285ea40c
Akshat Thakur <codingontheway@gmail.com>
2026-03-19 10:22:08 +0530

    Add Q1 - call add(int a, int b) in main() (level2)

--- src/methods/level2/Q1.java
+++ src/methods/level2/Q1.java
@@ -1,4 +1,19 @@
+// add(int a, int b) method banao jo sum print kare.
 package methods.level2;
+import java.util.Scanner;
 
 public class Q1 {
+    void add(int a, int b){
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Q1 obj = new Q1();
+        System.out.println("Enter value of a and b: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        obj.add(a,b);
+    }
 }
