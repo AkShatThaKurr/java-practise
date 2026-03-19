419d7c5e12204d61016576b8fc304196488ce71a
Akshat Thakur <codingontheway@gmail.com>
2026-03-19 10:15:33 +0530

    Add Q4 - call showNumber() in main() (level1)

---
+++ src/methods/level1/Q4.java
@@ -0,0 +1,13 @@
+// showNumber() method banao jo 100 print kare.
+package methods.level1;
+
+public class Q4 {
+    void showNumber(){
+        System.out.println(100);
+    }
+
+    public static void main(String[] args) {
+        Q4 obj = new Q4();
+        obj.showNumber();
+    }
+}
