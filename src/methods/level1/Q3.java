67f612641371c999683d00809d6c882336eb7684
Akshat Thakur <codingontheway@gmail.com>
2026-03-19 10:09:53 +0530

    Add Q3 - call line() 3 time in main() (level1)

---
+++ src/methods/level1/Q3.java
@@ -0,0 +1,17 @@
+// line() method banao jo "------------" print kare.
+package methods.level1;
+
+public class Q3 {
+    void line(){
+        System.out.println("------------");
+    }
+
+    public static void main(String[] args) {
+        Q3 obj = new Q3();
+        obj.line();
+        obj.line();
+        obj.line();
+
+    }
+
+}
