4904d7010e4c21be9c2dc22dc1d8393c9f1ca90c
Akshat Thakur <codingontheway@gmail.com>
2026-03-19 10:54:41 +0530

    Add Q4 - call table(int nu) in main() (level2)

---
+++ src/methods/level2/Q4.java
@@ -0,0 +1,20 @@
+// table(int n) method banao jo kisi number ka table print kare.
+package methods.level2;
+import java.util.Scanner;
+
+public class Q4 {
+    void table(int n){
+        System.out.println("The table of " + n + " is:");
+        for(int i=1; i<=10; i++){
+            System.out.println(n + "x" + i + "=" + n*i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Q4 obj = new Q4();
+        System.out.print("Enter a number of which table you want to print: ");
+        int num = sc.nextInt();
+        obj.table(num);
+    }
+}
