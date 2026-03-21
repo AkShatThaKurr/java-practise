71c949ab51450abb8ad0781d400c08b414c37fe5
Akshat Thakur <codingontheway@gmail.com>
2026-03-21 15:57:34 +0530

    Print Day name using Switch()

---
+++ src/Switch.java
@@ -0,0 +1,38 @@
+// print which is the day today using Switch Case.
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number between 1 to 7.");
+        int day = sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Today is Monday.");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday.");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday.");
+                break;
+            case 4:
+                System.out.println("Today is Thursday.");
+                break;
+            case 5:
+                System.out.println("Today is Friday.");
+                break;
+            case 6:
+                System.out.println("Today is Saturday.");
+                break;
+            case 7:
+                System.out.println("Today is Sunday.");
+                break;
+            default:
+                System.out.println("Invalid input.");
+        }
+        sc.close();
+    }
+
+}
