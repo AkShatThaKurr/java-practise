a5be7e69b1ba42c47131a49feae46367c05ff040
Akshat Thakur <codingontheway@gmail.com>
2026-03-23 00:18:25 +0530

    Add program for default (access modifier)

---
+++ src/accessmodifiers/c_default/Q1.java
@@ -0,0 +1,14 @@
+// Create a class Student with a variable name (default access). Create another class in the same package and print the name using an object.
+package accessmodifiers.c_default;
+
+class Student {
+    String name = "Akshat";
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println("Student Name: " + s.name);
+
+    }
+}
