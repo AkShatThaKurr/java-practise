ce9d5329bba0c40f30958b25770702782a6ca704
Akshat Thakur <codingontheway@gmail.com>
2026-03-23 20:23:04 +0530

    Add Q2 length() - (Method of String)

---
+++ src/strings/methodsinstring/method2length.java
@@ -0,0 +1,12 @@
+// Program to demonstrate length() method in String
+// length() returns ->  number of characters in the string
+
+package strings.methodsinstring;
+
+public class method2length {
+    public static void main(String[] args) {
+        String name = "akshat";
+        int len = name.length();
+        System.out.println("Length of " + name + " is: " + len);
+    }
+}
