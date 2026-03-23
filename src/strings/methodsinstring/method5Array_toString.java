07e51db6e58328c5491be0c22a0e53cc61644bdf
Akshat Thakur <codingontheway@gmail.com>
2026-03-24 01:06:55 +0530

    Add Q5 Arrays.toString() - (Method of String)

---
+++ src/strings/methodsinstring/method5Array_toString.java
@@ -0,0 +1,12 @@
+// Program to demonstrate Arrays.toString() method
+// We need to import java.util.Arrays to use Arrays.toString() method
+package strings.methodsinstring;
+
+import java.util.Arrays;
+
+public class method5Array_toString {
+    public static void main(String[] args) {
+        char[] arr = {'a', 'k', 's', 'h', 'a', 't'};
+        System.out.println(Arrays.toString(arr));
+    }
+}
