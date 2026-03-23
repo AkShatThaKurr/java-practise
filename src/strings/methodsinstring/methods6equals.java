766b167e1deb0460b32024562c294c2278369fd0
Akshat Thakur <codingontheway@gmail.com>
2026-03-24 01:28:23 +0530

    Add Q6 equals() - (Method of String)

---
+++ src/strings/methodsinstring/methods6equals.java
@@ -0,0 +1,14 @@
+// Demonstration of equals() method in String
+
+package strings.methodsinstring;
+
+public class methods6equals {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = "hi";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+
+    }
+}
