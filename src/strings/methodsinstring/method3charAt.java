21a5dbd2b4bc86f1e338e6a2c9c896aae875bd21
Akshat Thakur <codingontheway@gmail.com>
2026-03-23 23:27:20 +0530

    Add Q3(a) charAt() - (Method of String)

---
+++ src/strings/methodsinstring/method3charAt.java
@@ -0,0 +1,11 @@
+//  Program to demonstrate charAt() method in String
+//  charAt(index) returns the character at the given index
+package strings.methodsinstring;
+
+public class method3charAt {
+    public static void main(String[] args) {
+        String name = "akshat";
+        System.out.println("Character at index 0: " + name.charAt(0));
+        System.out.println("Character at index 2: " + name.charAt(2));
+    }
+}
