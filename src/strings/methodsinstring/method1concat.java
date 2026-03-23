ff0bdc4cef22f31e889992c3a7578562f5466e5a
Akshat Thakur <codingontheway@gmail.com>
2026-03-23 20:15:10 +0530

    Add Q1 concat() - (Method of String)

---
+++ src/strings/methodsinstring/method1concat.java
@@ -0,0 +1,12 @@
+// Program to demonstrate concat() method in String
+//concat() creates a new string and does not modify the original string
+package strings.methodsinstring;
+
+public class method1concat {
+    public static void main(String[] args) {
+        String name = "akshat";
+        name = name.concat(" thakur");
+        System.out.println(name);
+
+    }
+}
