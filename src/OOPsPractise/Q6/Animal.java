957f311216882ced5c9e1e65291e67322b8383b3
Akshat Thakur <codingontheway@gmail.com>
2026-03-28 02:16:25 +0530

    Add Q6 (Inheritance) - (OOPs)

---
+++ src/OOPsPractise/Q6/Animal.java
@@ -0,0 +1,14 @@
+/*
+Animal & Dog
+Create Animal class with method eat().
+Create Dog class that extends Animal, add method bark().
+Call both methods from Dog object.
+ */
+
+package OOPsPractise.Q6;
+
+public class Animal {
+    void eat() {
+        System.out.println("Animal eats food.");
+    }
+}
---
+++ src/OOPsPractise/Q6/Dog.java
@@ -0,0 +1,8 @@
+package OOPsPractise.Q6;
+
+public class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks on the noise.");
+    }
+}
+
