2db38ff1a44bfb26a4e549d9019319c784cd7c20
Akshat Thakur <codingontheway@gmail.com>
2026-03-27 20:34:57 +0530

    Add Abstract Keyword (Abstraction) - (OOPs)

---
+++ src/OOPs_Concepts/Pillar4_Abstraction/JohnCena.java
@@ -0,0 +1,7 @@
+package OOPs_Concepts.Pillar4_Abstraction;
+
+public class JohnCena extends Character {
+    void specialAttack(){
+        System.out.println("Attitude Adjustment");
+    }
+}
---
+++ src/OOPs_Concepts/Pillar4_Abstraction/TheRock.java
@@ -0,0 +1,7 @@
+package OOPs_Concepts.Pillar4_Abstraction;
+
+public class TheRock extends Character {
+    void specialAttack(){
+        System.out.println("Rock Bottom");
+    }
+}
