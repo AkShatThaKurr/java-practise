4a2800b31e5e0c8ed82461602df67fe56c08f348
Akshat Thakur <codingontheway@gmail.com>
2026-03-27 22:53:38 +0530

    Add Q2 (OOPs Practise) - (OOPs)

---
+++ src/OOPsPractise/Q2/Main.java
@@ -0,0 +1,8 @@
+package OOPsPractise.Q2;
+
+public class Main {
+    public static void main(String[] args) {
+        Student obj = new Student("Akshat", 20, 95);
+        obj.display();
+    }
+}
---
+++ src/OOPsPractise/Q2/Student.java
@@ -0,0 +1,27 @@
+/* student Class with Constructor
+Create a class Student with name, rollNo, marks.
+Add a parameterized constructor to initialize all attributes.
+Write a method display() to print student details.*/
+
+package OOPsPractise.Q2;
+
+public class Student {
+    private String name;
+    private int rollNo;
+    private float marks;
+
+    public Student(String name, int rollNo, float marks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
+
+         public void display(){
+             System.out.println("Student detail: ");
+            System.out.println("Name: " + name);
+            System.out.println("RollNo: " + rollNo);
+            System.out.println("Marks: " + marks);
+        }
+
+
+}
