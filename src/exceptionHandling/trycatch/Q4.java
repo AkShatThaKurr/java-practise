ca5d9b9e5b56982d8f76051f7fd691359f3cd1f7
Akshat Thakur <codingontheway@gmail.com>
2026-03-29 20:17:42 +0530

    Add Q3 Methods(printStackTrace, getMessage(), toString() ) - (Exception)

---
+++ src/exceptionHandling/trycatch/Q4.java
@@ -0,0 +1,33 @@
+package exceptionHandling.trycatch;
+
+public class Q4 {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10 / 0; // Exception yaha aayega
+
+        } catch (Exception e) {
+
+            System.out.println("Using println(e):");
+            System.out.println(e);   // toString()
+
+            System.out.println("\nUsing getMessage():");
+            System.out.println(e.getMessage());
+
+            System.out.println("\nUsing printStackTrace():");
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+Using println(e):
+java.lang.ArithmeticException: / by zero
+
+Using getMessage():
+/ by zero
+
+Using printStackTrace():
+java.lang.ArithmeticException: / by zero
+	at exceptionHandling.trycatch.Q4.main(Q4.java:7)
+ */
