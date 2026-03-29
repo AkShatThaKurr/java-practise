7617985b340bddecfd72dedb5cc641a5c11167c3
Akshat Thakur <codingontheway@gmail.com>
2026-03-29 19:58:51 +0530

    Add Q1 ArithmeticException - (Exception)

---
+++ src/exceptionHandling/trycatch/Q1.java
@@ -0,0 +1,16 @@
+// Exception: (ArithmeticException: / by zero)
+
+package exceptionHandling.trycatch;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Before Exception:");
+        try{
+            System.out.println(10/0);      // Exception
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println("After Exception:");
+    }
+}
