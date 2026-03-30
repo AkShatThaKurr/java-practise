5efe4813904f459e3a158248163a931edc644924
Akshat Thakur <codingontheway@gmail.com>
2026-03-30 18:42:56 +0530

    Add Q1 finally keyword  - (Exception )

---
+++ src/exceptionHandling/finally_keyword/Q1.java
@@ -0,0 +1,16 @@
+// the finally block will be executed at all cost, even the catch has executed.
+
+package exceptionHandling.finally_keyword;
+
+public class Q1 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Inside try");
+            System.out.println(10 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Inside catch");
+        } finally {
+            System.out.println("Inside finally");
+        }
+    }
+}
---
+++ src/exceptionHandling/finally_keyword/Q2.java
@@ -0,0 +1,19 @@
+// finally block executes even when we write return in the try block.
+
+package exceptionHandling.finally_keyword;
+
+public class Q2 {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Inside try");
+            return;
+        }
+        catch(Exception e){
+            System.out.println("Inside catch");
+        }
+        finally{
+            System.out.println("Inside finally");
+        }
+
+    }
+}
