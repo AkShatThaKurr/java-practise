6f17d011af418aa1c1dec2b10793e133026bff6c
Akshat Thakur <codingontheway@gmail.com>
2026-03-31 21:09:04 +0530

    Add P3: Taking input and storing in ArrayList (ArrayList<>) - (List<>) - (Collections)

---
+++ src/collection/Lect2/List/P3.java
@@ -0,0 +1,36 @@
+// Take input and store in ArrayList
+
+package collection.Lect2.List;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter of integer elements: ");
+        int num = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>(num);
+
+        System.out.println("Enter numbers:");
+
+        for (int i = 0; i < num; i++) {
+            int a = sc.nextInt();
+            list.add(a);
+        }
+        System.out.println(list);
+    }
+}
+
+/* Output:
+Enter of integer elements: 5
+Enter numbers:
+10
+20
+40
+50
+70
+[10, 20, 40, 50, 70]
+ */
