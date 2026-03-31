44f06b705ddb53136e508a56d76ff86d7ac2d166
Akshat Thakur <codingontheway@gmail.com>
2026-03-31 20:54:03 +0530

    Add P1: Using method remove() (ArrayList<>) - (List<>) - (Collections)

---
+++ src/collection/Lect2/List/P2.java
@@ -0,0 +1,31 @@
+// Remove element from ArrayList
+
+package collection.Lect2.List;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class P2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.print("The Array list is: " + list);
+        System.out.println("\n");
+
+        // Removing element from index 1.
+        list.remove(1);
+        System.out.print("New Array list is: " + list);
+        System.out.println();
+    }
+}
+
+/* Output:
+The Array list is: [10, 20, 30, 40, 50]
+
+New Array list is: [10, 30, 40, 50]
+
+ */
