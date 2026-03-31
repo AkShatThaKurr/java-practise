7c99e7300861ff4e8461c22ed32befcf0d0b9dc8
Akshat Thakur <codingontheway@gmail.com>
2026-03-31 20:42:23 +0530

    Add P3: ArrayList<> - (List<>) - (Collections)

---
+++ src/collection/Lect2/List/P1.java
@@ -0,0 +1,19 @@
+package collection.Lect2.List;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class P1 {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();  // Initial Capacity of ArrayList<> is 10,
+        arr.add(5);                             //after having 10 elements capacity changes to 15
+        arr.add(10);                            // new capacity = old capacity + (old capacity/2)
+        arr.add(15);
+        arr.add(20);
+        arr.add(25);
+        System.out.println(arr);        // Output: [5, 10, 15, 20, 25]
+
+        // We can also give capacity manually:
+        List<Integer> arr2 = new ArrayList<>(20);   // capacity of ArrayList is 20.
+    }
+}
