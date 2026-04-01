c8212d326cb8dd3358cae89e231ef2ceb3af74e6
Akshat Thakur <codingontheway@gmail.com>
2026-04-01 16:31:52 +0530

    Add P3: Add at specific index  (LinkedList<>) - (List<>) - (Collections)

---
+++ src/collection/Lect3_List/LinedList/P3.java
@@ -0,0 +1,24 @@
+// Add at specific index
+
+package collection.Lect3_List.LinedList;
+
+import java.util.LinkedList;
+
+public class P3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+
+        // suppose we need to add 25 at index 2. -->   list.add(index, element);
+        list.add(2, 25);
+        System.out.println(list);
+    }
+}
+
+/* Output:
+[10, 20, 30]
+[10, 20, 25, 30]
+ */
