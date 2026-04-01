c457b79669b16b61c9d53a6ed8a123d4756d5b26
Akshat Thakur <codingontheway@gmail.com>
2026-04-01 17:15:52 +0530

    Add P6: First and Last Access  (LinkedList<>) - (List<>) - (Collections)

---
+++ src/collection/Lect3_List/LinedList/P6.java
@@ -0,0 +1,23 @@
+// First & Last access
+
+package collection.Lect3_List.LinedList;
+import java.util.LinkedList;
+
+public class P6 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(100);
+        list.add(500);
+        list.add(800);
+
+        // to access first and last: -->   list.getFirst();    and    list.getLast();
+
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+    }
+}
+
+/*
+100
+800
+ */
