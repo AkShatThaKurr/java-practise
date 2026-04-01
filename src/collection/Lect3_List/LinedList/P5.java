82fa3f06f382c1aba6fa403bf4baf209059f2541
Akshat Thakur <codingontheway@gmail.com>
2026-04-01 16:56:43 +0530

    Add P3: Checking existence of element in LinkedLilst  (LinkedList<>) - (List<>) - (Collections)

---
+++ src/collection/Lect3_List/LinedList/P5.java
@@ -0,0 +1,27 @@
+//  Check element exists
+
+package collection.Lect3_List.LinedList;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class P5 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(100);
+        list.add(200);
+        list.add(300);
+        System.out.println(list);
+
+        // to check whether element exist or not: --> list.contains(val);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (list.contains(num)) {
+            System.out.println("Element Present.");
+        } else {
+            System.out.println("Element Absent.");
+        }
+    }
+}
