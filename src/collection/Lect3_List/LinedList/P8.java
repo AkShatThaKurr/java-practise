c2927b862b75d62f62af9fd02f249ffb7f12879d
Akshat Thakur <codingontheway@gmail.com>
2026-04-01 17:26:27 +0530

    Add P6: Count Even numbers in LinkedList  (LinkedList<>) - (List<>) - (Collections)

---
+++ src/collection/Lect3_List/LinedList/P8.java
@@ -0,0 +1,28 @@
+/*
+🔴 Level 3 (Interview level 🔥)
+✅ Q8. Count even numbers
+ */
+
+package collection.Lect3_List.LinedList;
+
+import java.util.LinkedList;
+
+public class P8 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(100);
+        list.add(101);
+        list.add(102);
+        list.add(103);
+        list.add(104);
+        System.out.println(list);
+
+        int countEven = 0;
+        for (int num : list) {
+            if (num % 2 == 0) {
+                countEven++;
+            }
+        }
+        System.out.println("Even numbers in list: " + countEven);
+    }
+}
