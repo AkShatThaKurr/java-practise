3362c3b3adadb3162497dd318547cd0e784f543b
Akshat Thakur <codingontheway@gmail.com>
2026-04-01 16:08:26 +0530

    Add P1: Using add() in ArrayList (ArrayList<>) - (List<>) - (Collections)

--- src/collection/Lect2_List/ArrayList/P1.java
+++ src/collection/Lect2_List/ArrayList/P1.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class P1 {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>();  // Initial Capacity of ArrayList<> is 10,
-        arr.add(5);                             //after having 10 elements capacity changes to 15
+        arr.add(5);                             // after having 10 elements capacity changes to 15
         arr.add(10);                            // new capacity = old capacity + (old capacity/2)
         arr.add(15);
         arr.add(20);
