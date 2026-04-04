e536375b582abfd047417ec30ad449fa16d30dd9
Akshat Thakur <codingontheway@gmail.com>
2026-04-04 16:22:03 +0530

    Add P1: Enumeration (Type1 of Cursor) -  (Cursor) - (Collections)

---
+++ src/collection/Lect4/Cursor/Enumeration/P1.java
@@ -0,0 +1,23 @@
+// Cursor: It is a tool which is used to traverse or iterate through the Collectins.
+// 1) Enumeration: It is used for legacy classes and can only read elements from collections no Update is allowed.
+
+package collection.Lect4.Cursor.Enumeration;
+
+import java.util.Vector;
+import java.util.Enumeration;
+
+public class P1 {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>();
+        v.add(10);
+        v.add(20);
+        v.add(30);
+        v.add(40);
+        v.add(50);
+
+        Enumeration<Integer> en = v.elements();
+        while(en.hasMoreElements()){
+            System.out.print(en.nextElement() + " ");
+        }
+    }
+}
