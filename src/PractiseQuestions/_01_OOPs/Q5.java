afeae2b7d0bcb7239ad1cb4c67577fc8162d2e96
Akshat Thakur <codingontheway@gmail.com>
2026-04-07 04:37:28 +0530

    Q5. Implement Single Inheritance

---
+++ src/PractiseQuestions/_01_OOPs/Q5.java
@@ -0,0 +1,37 @@
+/*
+Question: Implement Single Inheritance
+
+Q. Write a Java program to demonstrate single inheritance using a real-life example of Animal and Dog.
+
+Create a class Animal with a method eat().
+Create a class Dog that extends Animal and adds a method bark().
+In the main method, create an object of Dog and call both eat() and bark() methods.
+ */
+
+package PractiseQuestions._01_OOPs;
+
+public class Q5 {
+    static class Animal
+
+    {
+        void eat () {
+        System.out.println("Animal is eating food.");
+    }
+    }
+
+    static class Dog extends
+
+    Animal {
+        void bark () {
+            System.out.println("Dog is Barking.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Dog obj = new Dog();
+        obj.eat();
+        obj.bark();
+
+
+    }
+}
