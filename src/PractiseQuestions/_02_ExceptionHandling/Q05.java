c5b928f2ea3ddaa911dc3a8a7435fd854d41394f
Akshat Thakur <codingontheway@gmail.com>
2026-04-10 19:16:27 +0530

    Q04. throw keyword - (Exception handling)

---
+++ src/PractiseQuestions/_02_ExceptionHandling/Q05.java
@@ -0,0 +1,29 @@
+// Demonstrate throw keyword.
+
+package PractiseQuestions._02_ExceptionHandling;
+
+public class Q05 {
+    public static void main(String[] args) {
+
+        try {
+            throw new ArithmeticException(("Error"));
+        } catch (ArithmeticException e) {
+            System.out.println("Error handled");
+        }
+        System.out.println("After catch");
+        System.out.println();
+        int age = 13;
+        if (age < 18) {
+            throw new ArithmeticException(("Not allowed"));
+        }
+        // this line will not execute
+        System.out.println("after throw next line does not executes.");
+    }
+}
+
+// Output:
+// Error handled
+//After catch
+//
+//Exception in thread "main" java.lang.ArithmeticException: Not allowed
+//	at PractiseQuestions._02_ExceptionHandling.Q05.main(Q05.java:18)
