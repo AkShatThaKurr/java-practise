763353d1972f02cc95a7c29532973e06cb50ed21
Akshat Thakur <codingontheway@gmail.com>
2026-04-10 20:18:58 +0530

    Q09. Custom Exception Creation - (Password Verification) - (Exception handling)

---
+++ src/PractiseQuestions/_02_ExceptionHandling/Q09.java
@@ -0,0 +1,46 @@
+// Login system (invalid password exception).
+
+package PractiseQuestions._02_ExceptionHandling;
+
+import java.util.Scanner;
+
+public class Q09 {
+
+    // Custom Exception
+    static class InvalidPasswordException extends Exception {
+        InvalidPasswordException(String msg) {
+            super(msg);
+        }
+    }
+
+    static void passwordDetail(String id, String pass) throws InvalidPasswordException {
+
+        String username = "Abc";
+        String password = "123";
+
+        if (username.equals(id) && password.equals(pass)) {
+            System.out.println("Proceeding to Home page...");
+        } else {
+            throw new InvalidPasswordException("Invalid Username/Password entered!!!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter user name: ");
+        String id = sc.nextLine();
+
+        System.out.print("Enter password: ");
+        String pass = sc.nextLine();
+
+        try {
+            passwordDetail(id, pass);
+        } catch (InvalidPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
