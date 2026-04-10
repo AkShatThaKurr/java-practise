03066039960a02bfe068cb73932f4f7347bb2f6a
Akshat Thakur <codingontheway@gmail.com>
2026-04-10 19:30:55 +0530

    Q05. throws keyword - (Exception handling)

---
+++ src/PractiseQuestions/_02_ExceptionHandling/Q06.java
@@ -0,0 +1,20 @@
+// Demonstrate throws keyword.
+// throws keyword declares that the exception might occur.
+
+package PractiseQuestions._02_ExceptionHandling;
+
+public class Q06 {
+    static void test(int age) throws ArithmeticException {
+        if (age < 18) {
+            throw new ArithmeticException(("Not allowed"));
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            test(12);
+        } catch (ArithmeticException e) {
+            System.out.println("Handled in Main using try catch");
+        }
+    }
+}
