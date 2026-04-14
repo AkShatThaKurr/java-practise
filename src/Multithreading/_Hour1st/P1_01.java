935a17b5b91f482e67339582ffb2b9824943aced
Akshat Thakur <codingontheway@gmail.com>
2026-04-15 05:01:06 +0530

    P1_01 is a child thread class that extends Thread and overrides run() method. - (Multithreading using Thread class) - (Multithreading)

---
+++ src/Multithreading/_Hour1st/P1_01.java
@@ -0,0 +1,11 @@
+// Thread creation using extending Thread class and  ( run(), start() method).
+package Multithreading._Hour1st;
+
+
+public class P1_01 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Inside Child class " + i);
+        }
+    }
+}
