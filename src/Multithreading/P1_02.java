a99a7c8247cdb32e90841d543a0ab9722fa314f9
Akshat Thakur <codingontheway@gmail.com>
2026-04-15 15:05:57 +0530

    P1_02: Program for creating thread by using extend class. - (Multithreading using Thread class) - (Multithreading)

--- src/Multithreading/P1_02.java
+++ src/Multithreading/P1_02.java
@@ -1,6 +1,6 @@
 // Thread creation using extending Thread class and  ( run(), start() method).
 
-package Multithreading._;
+package Multithreading;
 
 public class P1_02 {
     public static void main(String[] args) {
